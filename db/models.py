a954479293c5e07bb9efef32ce6ca0f328928f30
z-rivald <127426656+z-rivald@users.noreply.github.com>
2025-01-07 09:20:06 +0500

    Add files via upload

---
+++ db/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy.sql.sqltypes import Integer, String, Boolean
+from db.database import Base
+from sqlalchemy import Column
+
+class DbUser(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    email = Column(String)
+    password = Column(String)
